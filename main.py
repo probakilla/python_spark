f35b574cdd5021b91208779db9985a3ec2dc774d
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2018-11-30 11:05:40 +0100

    fin du tp

--- main.py
+++ main.py
@@ -17,18 +17,12 @@ def transform_string(cityString):
     return (splitted[CITY_COL], pop)
 
 
-def open_hdfs(file):
-    ''' Retrieves a RDD of the worldcitiespop.original file '''
-    sc = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
-    return sc.textFile("hdfs://" + file)
-
-
-def tuplify_city(file):
-    rdd_file = open_hdfs(file)
-    tuple_2 = rdd_file.map(transform_string)
+def tuplify_city(file_rdd):
+    tuple_2 = file_rdd.map(transform_string)
     valid = tuple_2.filter(
         lambda city: city[CITY_COL] != NO_POP)
     return valid
+
 
 def filter_city(line):
     splitted = line.split(",")
@@ -58,16 +52,39 @@ def histogram(city_rdd):
                   .sortByKey() \
                   .map(lambda value: (int(math.pow(10, value[0])), value[1]))
 
-def join(cities_file, regions_file):
-    cities_rdd = open_hdfs(cities_file)
-    regions_rdd = open_hdfs(regions_file)
-    filtered_cities = cities_rdd.filter(filter_city)
 
+def join(cities_file, regions_file):
+    filtered_cities = cities_file.filter(filter_city)
+    mapped_cities = filtered_cities.map(filter_join_cities)
+    mapped_regions = regions_file.map(filter_join_regions)
+    joined_rdd = mapped_cities.join(mapped_regions)
+    return joined_rdd.map(map_cities_regions)
+
+
+def filter_join_cities(city_line):
+    splitted = city_line.split(",")
+    key = splitted[0].upper() + "," + splitted[3]
+    return (key, city_line)
+
+
+def filter_join_regions(region_line):
+    splitted = region_line.split(",")
+    key = splitted[0].upper() + "," + splitted[1]
+    return (key, region_line)
+
+def map_cities_regions(line):
+    splitted_cities = line[1][0].split(",")
+    splitted_regions = line[1][1].split(",")
+    return (splitted_cities[1], splitted_regions[2])
 
 if __name__ == "__main__":
-    valid_cities = tuplify_city(sys.argv[1])
+    sc = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
+    world_cities_file = sc.textFile("hdfs://" + sys.argv[1])
+    region_codes_file = sc.textFile("hdfs://" + sys.argv[2])
+    # valid_cities = tuplify_city(world_cities_file)
     # print("There is {:d} valid lines".format(valid_cities.count()))
     # print(cities_stats(valid_cities))
-    histogram_rdd = histogram(valid_cities)
-    for line in histogram_rdd.collect():
+    # histogram_rdd = histogram(valid_cities)
+    join_rdd = join(world_cities_file, region_codes_file)
+    for line in join_rdd.take(10):
         print(line)
