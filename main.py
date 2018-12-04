2300d247471282689392efd28d7f78faf39feb13
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2018-12-04 12:46:23 +0100

    swagg

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import sys
 import math
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.stat import Statistics
+from operator import add
 
 NO_POP = -1
 POP_COL = 4
+REG_CODE_COL = 3
+CSV_REG_CODE = 1
 CITY_COL = 1
-CLUSTER = "yarn-cluster"
+CLUSTER = "yarn"
 APP_NAME = "python_spark"
 
 
-def transform_string(cityString):
-    splitted = cityString.split(",")
+def transform_string(city_string):
+    splitted = city_string.split(",")
     valid_pop = (splitted[POP_COL] and splitted[POP_COL].isdigit())
     pop = NO_POP if not valid_pop else splitted[POP_COL]
     return (splitted[CITY_COL], pop)
@@ -47,44 +50,97 @@ def cities_stats(city_rdd):
 
 
 def histogram(city_rdd):
-    pop_rdd = city_rdd.map(lambda city: (int(math.log10(float(city[1]))), 1))
+    valid_cities = tuplify_city(city_rdd)
+    pop_rdd = valid_cities.map(lambda city: (
+        int(math.log10(float(city[1]))), 1))
     return pop_rdd.reduceByKey(lambda accum, n: accum + n) \
                   .sortByKey() \
                   .map(lambda value: (int(math.pow(10, value[0])), value[1]))
 
 
 def join(cities_file, regions_file):
-    filtered_cities = cities_file.filter(filter_city)
-    mapped_cities = filtered_cities.map(filter_join_cities)
-    mapped_regions = regions_file.map(filter_join_regions)
-    joined_rdd = mapped_cities.join(mapped_regions)
-    return joined_rdd.map(map_cities_regions)
+    mapped_cities = cities_file.filter(filter_city) \
+                               .map(mapper_join_cities)
+    mapped_regions = regions_file.map(mapper_join_regions)
+    return mapped_cities.join(mapped_regions) \
+                        .reduceByKey(
+                            lambda accum, x:
+                            accum if accum[1][POP_COL] > x[1][POP_COL] else x) \
+                        .map(map_cities_regions) \
+                        .sortByKey()
 
 
-def filter_join_cities(city_line):
+def mapper_join_cities(city_line):
     splitted = city_line.split(",")
-    key = splitted[0].upper() + "," + splitted[3]
+    key = splitted[0].upper() + "," + splitted[REG_CODE_COL]
     return (key, city_line)
 
 
-def filter_join_regions(region_line):
+def mapper_join_regions(region_line):
     splitted = region_line.split(",")
-    key = splitted[0].upper() + "," + splitted[1]
+    key = splitted[0].upper() + "," + splitted[CSV_REG_CODE]
     return (key, region_line)
+
 
 def map_cities_regions(line):
     splitted_cities = line[1][0].split(",")
     splitted_regions = line[1][1].split(",")
     return (splitted_cities[1], splitted_regions[2])
 
-if __name__ == "__main__":
+
+def main_join():
     sc = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
-    world_cities_file = sc.textFile("hdfs://" + sys.argv[1])
-    region_codes_file = sc.textFile("hdfs://" + sys.argv[2])
-    # valid_cities = tuplify_city(world_cities_file)
-    # print("There is {:d} valid lines".format(valid_cities.count()))
-    # print(cities_stats(valid_cities))
-    # histogram_rdd = histogram(valid_cities)
-    join_rdd = join(world_cities_file, region_codes_file)
-    for line in join_rdd.take(10):
+    num_executor = int(sc.getConf().get("spark.executor.instances"))
+    world_cities_file = sc.textFile("hdfs://" + sys.argv[1],
+                                    minPartitions=num_executor)
+    region_codes_file = sc.textFile("hdfs://" + sys.argv[2],
+                                    minPartitions=num_executor)
+    joined_rdd = join(world_cities_file, region_codes_file)
+    for line in joined_rdd.take(10):
         print(line)
+
+
+def main_tuple_2():
+    spark_context = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
+    num_executor = int(spark_context.getConf().get("spark.executor.instances"))
+    world_cities_file = spark_context.textFile("hdfs://" + sys.argv[1],
+                                               minPartitions=num_executor)
+    tuple_2 = tuplify_city(world_cities_file)
+    for line in tuple_2.take(10):
+        print(line)
+
+
+def main_stats():
+    spark_context = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
+    num_executor = int(spark_context.getConf().get("spark.executor.instances"))
+    world_cities_file = spark_context.textFile("hdfs://" + sys.argv[1],
+                                               minPartitions=num_executor)
+    tuplified_cities = tuplify_city(world_cities_file)
+    print(cities_stats(tuplified_cities))
+
+
+def main_hist():
+    spark_context = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
+    num_executor = int(spark_context.getConf().get("spark.executor.instances"))
+    world_cities_file = spark_context.textFile("hdfs://" + sys.argv[1],
+                                               minPartitions=num_executor)
+    histogram_rdd = histogram(world_cities_file)
+    for line in histogram_rdd.take(10):
+        print(line)
+
+
+if __name__ == "__main__":
+    # main_join()
+    # main_stats()
+    main_hist()
+
+# ex 30, cores 2, mem 2048M
+# join : 1m11, 50s
+# stats : 38s,
+# hist : 41s
+
+
+# ex 30, cores 4, mem 2048M
+# join
+# stats
+# hist
